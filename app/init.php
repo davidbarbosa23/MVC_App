3d9d995a699d3f2289133b83af014931cb88f750
David Barbosa <davidbarbosa.dev@gmail.com>
2018-11-14 15:50:14 -0500

    Redirect dorm home folder to public

--- app/init.php
+++ app/init.php
@@ -13,8 +13,8 @@ define('REQUEST_METHOD', $_SERVER['REQUEST_METHOD']);
 
 //Root URL 
 define('HTTP_ROOT',
-    (isset($_SERVER["HTTPS"]) ? "https" : "http") . '://' .$_SERVER['HTTP_HOST'] .
-    str_replace($_SERVER['DOCUMENT_ROOT'], '', str_replace('\\', '/', INC_ROOT).'/public')
+    (isset($_SERVER["HTTPS"]) ? "https" : "http") . '://' . $_SERVER['HTTP_HOST'] .
+    str_replace($_SERVER['DOCUMENT_ROOT'], '', str_replace('\\', '/', INC_ROOT) . '/public')
 );
 
 // Root path for assets
